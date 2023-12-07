659967a95af40181a2388cb0186b5c62c225c2e6
chenjiahan <chenjiahan.jait@bytedance.com>
2023-12-07 13:57:15 +0800

    chore: upd

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// import('./App').then((Modules) => {
-import(/* webpackChunkName: "bar" */'./App').then((Modules) => {
+import('./App').then((Modules) => {
+// import(/* webpackChunkName: "bar" */'./App').then((Modules) => {
   const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
     <React.StrictMode>
